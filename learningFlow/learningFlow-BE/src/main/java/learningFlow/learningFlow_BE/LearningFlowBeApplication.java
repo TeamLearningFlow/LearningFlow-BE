3d01e511aec0e6f38b08f6bfe293df28eadc0ef0
JungJaehoon0430 <80247965+JungJaehoon0430@users.noreply.github.com>
2025-01-08 18:41:13 +0900

    Initial project setup

---
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/LearningFlowBeApplication.java
@@ -0,0 +1,13 @@
+package learningFlow.learningFlow_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningFlowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningFlowBeApplication.class, args);
+	}
+
+}
---
+++ learningFlow/learningFlow-BE/src/test/java/learningFlow/learningFlow_BE/LearningFlowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package learningFlow.learningFlow_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningFlowBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
