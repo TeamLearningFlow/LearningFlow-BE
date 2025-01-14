7647f7f62d9d14463358e323ef229c68fae8d4f7
MODUGGAGI <kiroro0814@naver.com>
2025-01-14 12:43:15 +0900

    feat : springSecurity 사용시 ROLE에는 항상 앞에 "ROLE_" 이 붙어야 하기 때문에 key 값으로 추가

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/Role.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/Role.java
@@ -1,5 +1,12 @@
 package learningFlow.learningFlow_BE.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    ADMIN, USER
+    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+
+    private final String key;
 }
