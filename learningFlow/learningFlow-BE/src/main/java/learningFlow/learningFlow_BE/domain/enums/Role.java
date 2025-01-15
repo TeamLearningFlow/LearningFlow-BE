1b9e0695764bca055decf36ff0a61507e5f0974f
MODUGGAGI <kiroro0814@naver.com>
2025-01-15 15:14:57 +0900

    feat : ROLE에 키 값 삭제

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/Role.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/Role.java
@@ -6,7 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
-
-    private final String key;
+    ADMIN, USER
 }
