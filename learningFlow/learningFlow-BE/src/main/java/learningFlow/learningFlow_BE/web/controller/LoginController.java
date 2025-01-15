819a798a995d665c70cb78fe530357b54872adba
MODUGGAGI <kiroro0814@naver.com>
2025-01-15 15:13:48 +0900

    feat : 구글 로그인 시 기억하기 인증 사용위해 파라미터 추가, 비밀번호 재설정 용 API 추가, 로그아웃 용 API들 추가

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/web/controller/LoginController.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/web/controller/LoginController.java
@@ -2,6 +2,7 @@ package learningFlow.learningFlow_BE.web.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import learningFlow.learningFlow_BE.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,9 @@ import learningFlow.learningFlow_BE.service.user.UserService;
 import learningFlow.learningFlow_BE.web.dto.user.UserRequestDTO;
 import learningFlow.learningFlow_BE.web.dto.user.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -30,7 +34,7 @@ public class LoginController {
             @Valid @RequestBody UserRequestDTO.UserRegisterDTO request
     ) {
         return ApiResponse.onSuccess(authService.register(request));
-        //TODO: 회원가입 후 로그인 창으로 리다이렉트 하는게 나을것 같은데 이 부분은 아직 설정 안함...
+        //TODO: 회원가입 후 로그인 창으로 리다이렉트 하는게 나을것 같은데 이 부분은 아직 설정 안함(스웨거 테스트 시 불편)
     }
 
     @PostMapping("/login")
@@ -41,7 +45,7 @@ public class LoginController {
             HttpServletResponse httpResponse
     ) {
         return ApiResponse.onSuccess(authService.login(request, httpRequest, httpResponse));
-        //TODO: 로그인 후에도 /home으로 리다이렉트 되는게 나을 것 같은데 이 부분도 설정 안함
+        //TODO: 로그인 후에도 /home으로 리다이렉트 되는게 나을 것 같은데 이 부분 설정 안함(스웨거 테스트 시 불편)
     }
 
 
@@ -51,7 +55,13 @@ public class LoginController {
      */
     @GetMapping("/login/google")
     @Operation(summary = "구글 로그인 리다이렉트", description = "구글 로그인 페이지로 리다이렉트하는 API")
-    public void googleLogin(HttpServletResponse response) throws IOException {
+    public void googleLogin(
+            @RequestParam(defaultValue = "false") boolean rememberMe,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("OAUTH2_REMEMBER_ME", rememberMe);
 
         response.sendRedirect("/oauth2/authorization/google");
     }
@@ -83,5 +93,45 @@ public class LoginController {
             HttpServletRequest httpRequest) {  // HttpServletRequest 추가
 
         return ApiResponse.onSuccess(userService.updateAdditionalInfo(httpRequest, request));
+    }
+
+    @PostMapping("/find/password")
+    @Operation(summary = "비밀번호 재설정 요청 API", description = "이메일을 통해 비밀번호 재설정 링크를 전송하는 API")
+    public ApiResponse<String> requestPasswordReset(
+            @Valid @RequestBody UserRequestDTO.FindPasswordDTO request
+    ) {
+        authService.sendPasswordResetEmail(request);
+        return ApiResponse.onSuccess("이메일이 성공적으로 발송되었습니다.");
+    }
+
+    @PostMapping("/reset-password")
+    @Operation(summary = "비밀번호 재설정 API", description = "이메일로 받은 토큰을 통해 비밀번호를 재설정하는 API")
+    public ApiResponse<String> resetPassword(
+            @Valid @RequestBody UserRequestDTO.ResetPasswordDTO request
+    ) {
+        authService.resetPassword(request);
+        return ApiResponse.onSuccess("비밀번호 재설정이 완료되었습니다.");
+    }
+
+    @PostMapping("/logout")
+    @Operation(summary = "로그아웃 API", description = "로그아웃 실행하는 API, 실행 후 홈 화면으로 리다이렉트")
+    public String logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/home";
+    }
+
+    @PostMapping("/logout/test") // 테스트 전용 API
+    @Operation(summary = "로그아웃 테스트 용 API", description = "로그 아웃 테스트 후 리다이렉트 수행 안하고 스웨거에서 확인 가능하게 문자열 출력해주는 API")
+    public ResponseEntity<String> testLogout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return ResponseEntity.ok("로그아웃 성공");
     }
 }
