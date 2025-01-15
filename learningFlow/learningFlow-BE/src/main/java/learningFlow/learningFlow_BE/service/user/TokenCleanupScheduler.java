ab5414f7a8d277ac848efce72aba8bbb0381f732
MODUGGAGI <kiroro0814@naver.com>
2025-01-15 23:05:32 +0900

    feat : 회원가입 시 이메일 인증 처리 때 사용되는 토큰 스케쥴러로 자동으로 치우는 로직 추가

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/service/user/TokenCleanupScheduler.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/service/user/TokenCleanupScheduler.java
@@ -1,5 +1,6 @@
 package learningFlow.learningFlow_BE.service.user;
 
+import learningFlow.learningFlow_BE.repository.EmailVerificationTokenRepository;
 import learningFlow.learningFlow_BE.repository.PasswordResetTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,10 +12,16 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class TokenCleanupScheduler {
 
+    private final EmailVerificationTokenRepository emailVerificationTokenRepository;
     private final PasswordResetTokenRepository tokenRepository;
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 자정에 실행
     public void cleanupExpiredTokens() {
         tokenRepository.deleteByExpiryDateBefore(LocalDateTime.now());
+    }
+
+    @Scheduled(cron = "0 0 0 * * *") // 매일 자정에 실행
+    public void cleanupExpiredEmailVerificationTokens() {
+        emailVerificationTokenRepository.deleteByExpiryDateBefore(LocalDateTime.now());
     }
 }
