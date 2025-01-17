cffb7642618b7d7492892c79baaed661c74b9186
MODUGGAGI <kiroro0814@naver.com>
2025-01-18 00:55:41 +0900

    \feat : 구글 로그인은 항상 자동 로그인 되게 수정

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/security/handler/OAuth2LoginSuccessHandler.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/security/handler/OAuth2LoginSuccessHandler.java
@@ -1,11 +1,12 @@
-package learningFlow.learningFlow_BE.config.security.auth;
+package learningFlow.learningFlow_BE.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import learningFlow.learningFlow_BE.apiPayload.ApiResponse;
-import learningFlow.learningFlow_BE.config.security.jwt.JwtTokenProvider;
-import learningFlow.learningFlow_BE.service.user.OAuth2UserTemp;
+import learningFlow.learningFlow_BE.security.auth.PrincipalDetails;
+import learningFlow.learningFlow_BE.security.jwt.JwtTokenProvider;
+import learningFlow.learningFlow_BE.service.auth.oauth.OAuth2UserTemp;
 import learningFlow.learningFlow_BE.web.dto.user.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,28 +31,27 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException {
         log.info("OAuth2 로그인 성공!");
 
-        // Principal 타입 확인
+        // Principal 타입 확인, 첫 로그인인 경우 회원가입으로 이동
         if (authentication.getPrincipal() instanceof OAuth2UserTemp oAuth2UserTemp) {
-
             String temporaryToken = jwtTokenProvider.createTemporaryToken(oAuth2UserTemp);
-
             String redirectUrl = "/oauth2/additional-info?token=" + temporaryToken;
             response.sendRedirect(redirectUrl);
             return;
         }
 
+        //인증 정보 저장
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         // 기존 사용자인 경우
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+        //Access Token 생성
         String accessToken = jwtTokenProvider.createAccessToken(authentication);
-        String refreshToken = jwtTokenProvider.createRefreshToken(authentication);
-
-        log.info("Access Token: {}", accessToken);
-        log.info("Refresh Token: {}", refreshToken);
-
-        // 토큰을 헤더에 추가
+        log.info("Access 토큰 발급 : {}", accessToken);
         response.addHeader("Authorization", "Bearer " + accessToken);
+
+        String refreshToken = jwtTokenProvider.createRefreshToken(authentication);
+        log.info("자동 로그인 활성화, Refresh Token 발급 : {}", refreshToken);
         response.addHeader("Refresh-Token", refreshToken);
 
         // 헤더 설정 확인 로깅
