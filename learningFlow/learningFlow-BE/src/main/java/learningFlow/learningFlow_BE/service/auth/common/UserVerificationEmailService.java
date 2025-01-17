375274c762f5854e75de7920c5bc426ba4bf86e4
MODUGGAGI <kiroro0814@naver.com>
2025-01-18 00:52:24 +0900

    chore : 클래스 이름 수정 및 패키지 더 잘 어울리는 곳으로 변경

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/service/auth/common/UserVerificationEmailService.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/service/auth/common/UserVerificationEmailService.java
@@ -1,4 +1,4 @@
-package learningFlow.learningFlow_BE.service.user;
+package learningFlow.learningFlow_BE.service.auth.common;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class EmailService {
+public class UserVerificationEmailService {
 
     private final JavaMailSender emailSender;
 
