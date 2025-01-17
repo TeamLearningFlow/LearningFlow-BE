d37d037abdb869cc8c99879790a038f25e5d0ca3
MODUGGAGI <kiroro0814@naver.com>
2025-01-18 00:54:16 +0900

    feat : pm 요구 사항에 맞게 수정

--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/Job.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/Job.java
@@ -1,5 +1,18 @@
 package learningFlow.learningFlow_BE.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Job {
-    STUDENT, WORKER, NONE
+    STUDENT("대학생(휴학생)"),
+    ADULT("성인"),
+    EMPLOYEE("직장인"),
+    JOB_SEEKER("이직/취업 준비생"),
+    OTHER("기타");
+
+    private final String description;
+
+    Job(String description) {
+        this.description = description;
+    }
 }
--- learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/MediaType.java
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/domain/enums/MediaType.java
@@ -1,5 +1,16 @@
 package learningFlow.learningFlow_BE.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum MediaType {
-    VIDEO, TEXT, BOTH
+    NO_PREFERENCE("상관없음"),
+    VIDEO("영상이 좋아요"),
+    TEXT("텍스트가 좋아요");
+
+    private final String description;
+
+    MediaType(String description) {
+        this.description = description;
+    }
 }
