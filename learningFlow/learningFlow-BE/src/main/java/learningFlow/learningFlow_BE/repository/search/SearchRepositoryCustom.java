ed7b9d18480d61117c581501f7ae5853975d4cc0
MODUGGAGI <kiroro0814@naver.com>
2025-01-20 01:42:12 +0900

    feat : QueryDsl 사용 위한 Repository

---
+++ learningFlow/learningFlow-BE/src/main/java/learningFlow/learningFlow_BE/repository/search/SearchRepositoryCustom.java
@@ -0,0 +1,11 @@
+package learningFlow.learningFlow_BE.repository.search;
+
+import learningFlow.learningFlow_BE.domain.Collection;
+import learningFlow.learningFlow_BE.web.dto.search.SearchRequestDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface SearchRepositoryCustom {
+    List<Collection> searchCollections(SearchRequestDTO.SearchConditionDTO condition, Long lastId, Pageable pageable);
+}
