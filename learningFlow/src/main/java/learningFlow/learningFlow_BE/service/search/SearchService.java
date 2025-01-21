b75d070ab8a85150d00983cc56d1c7b52cc9d7f1
MODUGGAGI <kiroro0814@naver.com>
2025-01-22 02:57:57 +0900

    feat: 검색할 때에도 로그인한 경우 검색 결과에 북마크했는지 안했는지 여부 포함해주기위해 SearchService에 로직 추가

--- learningFlow/src/main/java/learningFlow/learningFlow_BE/service/search/SearchService.java
+++ learningFlow/src/main/java/learningFlow/learningFlow_BE/service/search/SearchService.java
@@ -2,11 +2,14 @@ package learningFlow.learningFlow_BE.service.search;
 
 import learningFlow.learningFlow_BE.converter.SearchConverter;
 import learningFlow.learningFlow_BE.domain.Collection;
+import learningFlow.learningFlow_BE.domain.User;
 import learningFlow.learningFlow_BE.repository.search.SearchRepositoryCustom;
+import learningFlow.learningFlow_BE.security.auth.PrincipalDetails;
 import learningFlow.learningFlow_BE.web.dto.search.SearchRequestDTO;
 import learningFlow.learningFlow_BE.web.dto.search.SearchResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,25 +21,30 @@ public class SearchService {
     private final SearchRepositoryCustom searchRepository;
     private static final int PAGE_SIZE = 8;
 
-    public SearchResponseDTO.SearchResultDTO search(SearchRequestDTO.SearchConditionDTO condition, Long lastId) {
+    public SearchResponseDTO.SearchResultDTO search(SearchRequestDTO.SearchConditionDTO condition, Long lastId, Authentication authentication) {
 
         PageRequest pageRequest = PageRequest.of(0, PAGE_SIZE);
         List<Collection> collections = searchRepository.searchCollections(condition, lastId, pageRequest);
 
 
         if (collections.isEmpty()) {
-            return SearchConverter.toSearchResultDTO(collections, null, false, 0, 0);
+            return SearchConverter.toSearchResultDTO(collections, null, false, 0, 0, null);
         }
 
         Long lastCollectionId = collections.getLast().getId();
-        boolean hasNext = hasNextPage(condition,lastCollectionId);
+        boolean hasNext = hasNextPage(condition, lastCollectionId);
 
         Integer totalCount = searchRepository.getTotalCount(condition);
         int totalPages = (int) Math.ceil((double) totalCount / PAGE_SIZE);
 
         int currentPage = calculateCurrentPage(lastId, condition);
 
-        return SearchConverter.toSearchResultDTO(collections, lastCollectionId, hasNext, totalPages, currentPage);
+        User currentUser = null;
+        if (authentication != null && authentication.getPrincipal() instanceof PrincipalDetails) {
+            currentUser = ((PrincipalDetails) authentication.getPrincipal()).getUser();
+        }
+
+        return SearchConverter.toSearchResultDTO(collections, lastCollectionId, hasNext, totalPages, currentPage, currentUser);
     }
 
     private int calculateCurrentPage(Long lastId, SearchRequestDTO.SearchConditionDTO condition) {
