a72fb568c5e16b446d07bb3cc9be418a7f3b079d
JungJaehoon0430 <80247965+JungJaehoon0430@users.noreply.github.com>
2025-01-24 23:57:31 +0900

    홈페이지 관련 merge된거 pull 후 수정
    .

--- learningFlow/src/main/java/learningFlow/learningFlow_BE/repository/UserCollectionRepository.java
+++ learningFlow/src/main/java/learningFlow/learningFlow_BE/repository/UserCollectionRepository.java
@@ -2,9 +2,17 @@ package learningFlow.learningFlow_BE.repository;
 
 import learningFlow.learningFlow_BE.domain.User;
 import learningFlow.learningFlow_BE.domain.UserCollection;
+import learningFlow.learningFlow_BE.domain.enums.UserCollectionStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserCollectionRepository extends JpaRepository<UserCollection, Long> {
+    List<UserCollection> findByUserAndStatusOrderByLastAccessedAtDesc(User user, UserCollectionStatus status);
+
+    @Query("SELECT uc FROM UserCollection uc WHERE uc.user = :user ORDER BY uc.lastAccessedAt DESC")
+    List<UserCollection> findRecentByUser(@Param("user") User user, Pageable pageable);
 }
