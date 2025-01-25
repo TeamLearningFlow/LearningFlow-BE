508f05fb136f0876d07ad277bdb0b87b13923c37
MODUGGAGI <kiroro0814@naver.com>
2025-01-25 14:42:15 +0900

    feat : home에 쓰일 DTO로 변환하는 컨버터 추가

--- learningFlow/src/main/java/learningFlow/learningFlow_BE/converter/HomeConverter.java
+++ learningFlow/src/main/java/learningFlow/learningFlow_BE/converter/HomeConverter.java
@@ -1,43 +1,17 @@
 package learningFlow.learningFlow_BE.converter;
 
-import learningFlow.learningFlow_BE.domain.Collection;
-import learningFlow.learningFlow_BE.domain.CollectionEpisode;
-import learningFlow.learningFlow_BE.web.dto.home.HomeResponseDTO.HomeInfoDTO;
-import learningFlow.learningFlow_BE.web.dto.home.HomeResponseDTO.RecentLearningDTO;
-import learningFlow.learningFlow_BE.web.dto.home.HomeResponseDTO.EpisodeDTO;
-import learningFlow.learningFlow_BE.web.dto.collection.CollectionResponseDTO.CollectionPreviewDTO;
+import learningFlow.learningFlow_BE.web.dto.collection.CollectionResponseDTO;
+import learningFlow.learningFlow_BE.web.dto.home.HomeResponseDTO;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HomeConverter {
-    public static HomeInfoDTO toHomeInfoDTO(List<CollectionPreviewDTO> recommendedCollections, RecentLearningDTO recentLearning) {
-        return HomeInfoDTO.builder()
-                .recommendedCollections(recommendedCollections)
-                .recentLearning(recentLearning)
-                .build();
-    }
 
-    public static RecentLearningDTO toRecentLearningDTO(Collection collection, int currentEpisode, boolean isCompleted, List<CollectionEpisode> episodes) {
-        return RecentLearningDTO.builder()
-                .collectionId(collection.getId())
-                .title(collection.getTitle())
-                .currentEpisode(currentEpisode)
-                .isCompleted(isCompleted)
-                .episodes(toEpisodeDTOList(episodes))
-                .build();
-    }
-
-    public static List<EpisodeDTO> toEpisodeDTOList(List<CollectionEpisode> episodes) {
-        return episodes.stream()
-                .map(HomeConverter::toEpisodeDTO)
-                .collect(Collectors.toList());
-    }
-
-    public static EpisodeDTO toEpisodeDTO(CollectionEpisode episode) {
-        return EpisodeDTO.builder()
-                .episodeNumber(episode.getEpisodeNumber())
-                .episodeName(episode.getEpisodeName())
+    public static HomeResponseDTO.GuestHomeInfoDTO convertToGuestHomeInfoDTO(
+            List<CollectionResponseDTO.CollectionPreviewDTO> collectionPreviewList
+    ) {
+        return HomeResponseDTO.GuestHomeInfoDTO.builder()
+                .recommendedCollections(collectionPreviewList)
                 .build();
     }
 }
