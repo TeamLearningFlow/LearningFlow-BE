b7a70170635ca8086a0c9f7d203df52dc8369b0e
MODUGGAGI <kiroro0814@naver.com>
2025-01-25 14:42:53 +0900

    feat : homeService 삭제로 CollectionService 통해서 Collection 조회

--- learningFlow/src/main/java/learningFlow/learningFlow_BE/web/controller/HomeRestController.java
+++ learningFlow/src/main/java/learningFlow/learningFlow_BE/web/controller/HomeRestController.java
@@ -6,10 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import learningFlow.learningFlow_BE.apiPayload.ApiResponse;
-import learningFlow.learningFlow_BE.domain.User;
 import learningFlow.learningFlow_BE.security.auth.PrincipalDetails;
-import learningFlow.learningFlow_BE.service.home.HomeService;
-import learningFlow.learningFlow_BE.web.dto.home.HomeResponseDTO.HomeInfoDTO;
+import learningFlow.learningFlow_BE.service.collection.CollectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Home", description = "홈 화면 보여주는 API")
 public class HomeRestController {
 
-    private final HomeService homeService;
+    private final CollectionService collectionService;
 
     @GetMapping
     @Operation(summary = "홈 화면 API", description = "홈 화면에 필요한 정보를 제공하는 API입니다.")
@@ -32,13 +30,15 @@ public class HomeRestController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER4001", description = "사용자를 찾을 수 없습니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
-    public ApiResponse<HomeInfoDTO> getHome(
+    public ApiResponse<?> getHome(
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        log.info("홈 화면 API 호출");
-        User user = principalDetails != null ? principalDetails.getUser() : null;
-        log.info("사용자 상태: {}", user != null ? "로그인" : "비로그인");
-        HomeInfoDTO homeInfo = homeService.getHomeInfo(user);
-        return ApiResponse.onSuccess(homeInfo);
+        if (principalDetails == null || principalDetails.getUser() == null) {
+            log.info("비회원");
+            return ApiResponse.onSuccess(collectionService.getGuestHomeCollections());
+        } else {
+            log.info("회원");
+            return ApiResponse.onSuccess(collectionService.getUserHomeCollections(principalDetails.getUser()));
+        }
     }
 }
