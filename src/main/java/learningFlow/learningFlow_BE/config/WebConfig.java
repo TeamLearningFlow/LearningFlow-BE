3132c761e7272bace68520d3136ffebd133d98fa
Karen <mingki080808@gmail.com>
2025-02-06 17:16:12 +0900

    [fix] onboarding url, 8080 url CORS 추가

--- src/main/java/learningFlow/learningFlow_BE/config/WebConfig.java
+++ src/main/java/learningFlow/learningFlow_BE/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8081")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://onboarding.p-e.kr:8080")
 //                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
