92775c8ce32e367a427f55264e5fff87a79f14b9
MODUGGAGI <kiroro0814@naver.com>
2025-02-14 02:48:39 +0900

    feat : 홈화면에서 recentlyCollection 조회 로직 수정

--- src/main/java/learningFlow/learningFlow_BE/domain/UserCollection.java
+++ src/main/java/learningFlow/learningFlow_BE/domain/UserCollection.java
@@ -33,7 +33,7 @@ public class UserCollection extends BaseEntity{
     @Column(name = "user_collection_status", nullable = false)
     private Integer userCollectionStatus; // 가장 최신에 수강한 강의 저장
 
-    @Column(name = "completed_time", nullable = false)
+    @Column(name = "completed_time")
     private LocalDate completedTime;
 
     @Enumerated(EnumType.STRING)
