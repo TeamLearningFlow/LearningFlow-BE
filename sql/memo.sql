3dd391c4f687ac27e30028c45a5e8b7882f615c3
Karen <mingki080808@gmail.com>
2025-02-22 18:05:51 +0900

    [BACKUP] sql 백업

---
+++ sql/memo.sql
@@ -0,0 +1,34 @@
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (19, '2025-02-18 07:39:01.344044', '2025-02-19 22:16:40.785421', '글 작성', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (32, '2025-02-15 20:11:37.447520', '2025-02-15 20:11:37.447520', 'episodeId=32
+노트', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (34, '2025-02-13 08:53:29.139924', '2025-02-15 20:02:13.447865', 'episodeNumber = 34
+노트', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (42, '2025-02-21 14:23:05.628987', '2025-02-21 15:53:34.661411', 'ㅇ러ㅣㅏㄴㅁ어ㅣ란', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (42, '2025-02-18 09:42:36.260585', '2025-02-18 09:42:36.260585', 'React공부 중입니다.', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (44, '2025-02-01 12:59:29.689820', '2025-02-01 15:04:02.823004', 'api 테스트 중입니다', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (52, '2025-02-18 11:45:37.710944', '2025-02-18 11:45:37.710944', '연정\'s 피그마 온보딩 정리', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (61, '2025-02-20 23:35:56.794800', '2025-02-20 23:35:56.794800', '노트 작성', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (66, '2025-02-18 19:43:24.730270', '2025-02-18 19:43:24.730270', 'ㅇㅁㄹㅇㄴ', 'LOCAL_acd4d557');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (68, '2025-02-21 16:25:05.034419', '2025-02-21 16:25:05.034419', 'hyuhy', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (69, '2025-02-21 16:25:13.501227', '2025-02-21 16:25:13.501227', 'hyju', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (86, '2025-02-21 10:15:15.074772', '2025-02-21 10:15:37.062089', '통계 짱 
+우와', 'LOCAL_6edc652f');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (96, '2025-02-21 13:36:41.912831', '2025-02-21 13:36:41.912831', 'qq', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (97, '2025-02-21 14:08:46.418330', '2025-02-21 14:08:46.418330', 'gg', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (97, '2025-02-18 10:03:08.123317', '2025-02-20 16:15:16.185552', 'JOIN 정상 작동', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (98, '2025-02-18 11:30:34.232599', '2025-02-18 11:30:34.232599', '2회차 테스트', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (100, '2025-02-18 08:26:31.392198', '2025-02-18 08:26:31.392198', '우와', 'LOCAL_2f9195c3');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (110, '2025-02-21 16:46:52.457612', '2025-02-21 16:46:52.457612', 'gg', 'LOCAL_fa9712ec');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (112, '2025-02-20 19:30:18.648010', '2025-02-20 19:30:18.648010', '2D 슈팅 프로젝트
+', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (112, '2025-02-21 12:50:44.109307', '2025-02-21 12:50:44.109307', '굳
+', 'LOCAL_b210c6da');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (119, '2025-02-21 06:12:04.581888', '2025-02-21 06:12:04.581888', '노트 작성 중', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (121, '2025-02-21 07:13:46.135189', '2025-02-21 07:13:46.135189', '내 서비스의 예상 고객은 누구일까?', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (159, '2025-02-19 20:14:08.475539', '2025-02-19 20:14:08.475539', '영어 컬렉션 추가', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (161, '2025-02-22 12:19:59.807594', '2025-02-22 12:19:59.807594', 'fjdhkdl', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (193, '2025-02-21 15:07:49.753132', '2025-02-21 15:07:49.753132', '완료', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (196, '2025-02-20 23:23:01.535373', '2025-02-20 23:23:01.535373', '노트 작성 중', 'LOCAL_540712bd');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (196, '2025-02-20 18:35:56.155090', '2025-02-20 18:35:56.155090', '코틀린 몰라!!!!', 'LOCAL_6252bc51');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (205, '2025-02-21 16:55:46.690016', '2025-02-21 16:55:46.690016', '34rq3r', 'LOCAL_ea15ac0e');
+INSERT INTO learningflowdb.memo (collection_episode_id, created_at, updated_at, contents, user_id) VALUES (205, '2025-02-21 16:42:57.655708', '2025-02-21 16:42:57.655708', 'djssydgjtodt', 'LOCAL_fa9712ec');
