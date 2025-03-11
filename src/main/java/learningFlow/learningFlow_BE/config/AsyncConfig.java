8c7d079641a6406b016c5fa8c71780a7620af422
MODUGGAGI <kiroro0814@naver.com>
2025-03-11 22:30:15 +0900

    feat : 이메일 전송 비동기 처리 @Async 어노테이션으로 간단하게 추가

---
+++ src/main/java/learningFlow/learningFlow_BE/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package learningFlow.learningFlow_BE.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync // 비동기 기능 활성화
+public class AsyncConfig {
+
+    @Bean(name = "emailTaskExecutor")
+    public Executor emailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);        // 기본 스레드 수
+        executor.setMaxPoolSize(5);         // 최대 스레드 수
+        executor.setQueueCapacity(100);     // 대기 큐 크기
+        executor.setThreadNamePrefix("EmailAsync-");
+        executor.initialize();
+        return executor;
+    }
+}
